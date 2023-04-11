e5b8f7c0d8de5992c281c24db3198a8d5994bd30
Elkhan Julian Brillianshah <ubbeg2000@gmail.com>
2023-04-11 15:11:01 +0700

    add: bikin scheduler buat clear data tiap dua hari

--- main.go
+++ main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
-	"os"
 	"server/models"
 	"server/setup"
 	"server/udp"
 	"server/web"
 	"sync"
 	"time"
+
+	"github.com/go-co-op/gocron"
 )
 
 func main() {
 	setup.Env()
 	setup.Database()
 	setup.ChannelServer()
-	args := os.Args
 
-	if len(args) == 1 {
-		wg := new(sync.WaitGroup)
-		wg.Add(2)
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(5).Days().Do(func() {
+		db := setup.DB
+		now := time.Now()
 
-		go func() {
-			udp.Run()
-			wg.Done()
-		}()
+		db.Delete(&models.AccessLog{}, "timestamp <= ?", now)
+		db.Delete(&models.RSSILog{}, "timestamp <= ?", now)
+		db.Delete(&models.AccessRule{}, "ends_at <= ?", now)
+	})
 
-		go func() {
-			web.Run()
-			wg.Done()
-		}()
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
 
-		wg.Wait()
-	} else {
-		cmd := args[1]
-		if cmd == "clear_logs" {
-			db := setup.DB
-			now := time.Now()
+	go func() {
+		udp.Run()
+		wg.Done()
+	}()
 
-			db.Delete(&models.AccessLog{}, "timestamp <= ?", now)
-			db.Delete(&models.RSSILog{}, "timestamp <= ?", now)
-		}
-	}
+	go func() {
+		web.Run()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
